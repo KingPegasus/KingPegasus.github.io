22ef3bcee23329599540181691d35a5ab1aef059
Muhammad Raza Khalid Saleemi <rks143@gmail.com>
2026-03-08 03:52:11 +0500

    Update site URL in configuration and add CNAME for custom domain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://kingpegasus.github.io",
+  site: "https://razasaleemi.com",
   integrations: [tailwind(), sitemap()],
 });
